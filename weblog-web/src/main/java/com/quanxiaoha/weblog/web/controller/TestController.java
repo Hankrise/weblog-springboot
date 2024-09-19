70efd9f2da560419515512800b60205c2019a963
Hankrise <hankrise@live.cn>
2024-09-19 22:08:28 +0800

    3.8 Spring Boot 实现优雅的参数校验 需要注意TestController.java，需要手动导入类，教程里缺失了

--- weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
@@ -3,9 +3,15 @@ package com.quanxiaoha.weblog.web.controller;
 import com.quanxiaoha.weblog.web.model.User;
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.validation.FieldError;
+import java.util.stream.Collectors;
 
 /**
  * @author: 犬小哈
@@ -19,9 +25,20 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
         // 返参
-        return user;
+        return ResponseEntity.ok("参数没有任何问题");
     }
 
 }
