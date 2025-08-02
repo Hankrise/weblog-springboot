df2633e2d2dfeebf3ec8541f6a8800b05757f79f
Hankrise <hankrise@live.cn>
2025-08-02 16:55:51 +0800

    5.2 登录页加点盐：通过 Animate.css 添加动画

--- weblog-vue3/src/pages/admin/login.vue
+++ weblog-vue3/src/pages/admin/login.vue
@@ -2,7 +2,7 @@
   <!-- 使用 grid 网格布局，并指定列数为 2，高度占满全屏 -->
   <div class="grid grid-cols-2 h-screen">
     <!-- 默认先适配移动端，占两列，order 用于指定排列顺序，md 用于适配非移动端（PC 端）；背景色为黑色 -->
-    <div class="col-span-2 order-2 p-10 md:col-span-1 md:order-1 bg-slate-900">
+    <div class="col-span-2 order-2 p-10 md:col-span-1 md:order-1 bg-slate-900 animate__animated animate__bounceInLeft animate__fast">
       <!-- 指定为 flex 布局，并设置为屏幕垂直水平居中，高度为 100% -->
       <div class="flex justify-center items-center h-full flex-col">
         <h2 class="font-bold text-4xl mb-7 text-white">Weblog 博客登录</h2>
@@ -11,7 +11,7 @@
         <img src="@/assets/developer.png" class="w-1/2">
       </div>
     </div>
-    <div class="col-span-2 order-1 md:col-span-1 md:order-2 bg-white">
+    <div class="col-span-2 order-1 md:col-span-1 md:order-2 bg-white animate__animated animate__bounceInRight animate__fast">
       <!-- flex-col 用于指定子元素垂直排列 -->
       <div class="flex justify-center items-center h-full flex-col">
         <!-- 大标题，设置字体粗细、大小、下边距 -->
